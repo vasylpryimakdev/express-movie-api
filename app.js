2b1342d2428755daabcc6dd1f61b7f136e3e3e03
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-21 17:30:30 +0300

    refactor: replace users route with movie/search, add API key middleware

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ var logger = require("morgan");
 const helmet = require("helmet");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const movieRouter = require("./routes/movie");
+const searchRouter = require("./routes/search");
 
 var app = express();
 app.use(helmet());
 
-// view engine setup
+app.use((req, res, next) => {
+  if (req.query.api_key != 123456789) {
+    res.status(401);
+    res.json("Invalid API Key");
+  } else {
+    next();
+  }
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -22,20 +31,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/movie", movieRouter);
+app.use("/search", searchRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
