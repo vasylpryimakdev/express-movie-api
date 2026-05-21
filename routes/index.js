d8537a35086e0cacd4bab8d15326b9472d8bc6c1
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-05-21 17:40:58 +0300

    feat: add most_popular movies endpoint with pagination

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
+
+const movies = require("../data/movies");
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/most_popular", (req, res, next) => {
+  let page = req.query.page;
+
+  if (page === undefined) {
+    page = 1;
+  }
+
+  let results = movies.filter((movie) => {
+    return movie.most_popular;
+  });
+
+  const indexToStart = (page - 1) * 20;
+
+  results = results.slice(indexToStart, indexToStart + 19);
+  
+  res.json({
+    page,
+    results,
+  });
 });
 
 module.exports = router;
